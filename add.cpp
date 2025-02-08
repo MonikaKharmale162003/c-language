30628a5b76981e79bb57ea6dafae142f6a1a7714
Monika Kharmale <156334972+MonikaKharmale162003@users.noreply.github.com>
2025-02-08 21:18:40 +0530

    Add files via upload

---
+++ add.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b,sum;
+    cout<<"enter a number";
+    cin>>a>>b;
+    sum=a+b;
+   cout<<"sum=";
+   cout<<sum;
+   return 0;
+
+}
